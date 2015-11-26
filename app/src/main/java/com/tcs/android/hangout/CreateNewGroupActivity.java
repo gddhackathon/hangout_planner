b0fb3bed1cf4ea509e77702ec879da03a1978a78
gddhackathon <gdd.hackathon@gmail.com>
2015-11-25 21:17:32 -0600

    HangoutSample

---
+++ app/src/main/java/com/tcs/android/hangout/CreateNewGroupActivity.java
@@ -0,0 +1,42 @@
+package com.tcs.android.hangout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class CreateNewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_group);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.create_new_group_menu, menu);
+        return true;
+    }
+
+    public void addParticipants(View view){
+        Intent intent = new Intent(this, AddParticipantsActivity.class);
+        startActivity(intent);
+    }
+}
